f6ee428a8ae245ab5d023193b2014b8b9498a2b2
[Sean Sellers] <[sellers@nmsu.edu]>
2023-10-16 12:51:32 -0600

    Fixed noise

--- ssosoft/rosaZylaCal.py
+++ ssosoft/rosaZylaCal.py
@@ -236,7 +236,7 @@ class rosaZylaCal:
 			while i < self.burstNumber:
 				for flat in self.flatList:
 					with fits.open(flat) as hdu:
-						for hduExt in hdu[1:]
+						for hduExt in hdu[1:]:
 							self.noise[i, :, :] = hduExt.data - self.avgDark
 							i += 1
 		else:
