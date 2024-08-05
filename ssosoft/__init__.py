6aa5fba2a14d5b70eaa941a9de598027f77c9ae2
[Sean Sellers] <[sellers@nmsu.edu]>
2024-08-05 13:44:58 -0600

    Added HSG support, reorganized project into "imagers" and "spectra" for future FIRS integration.

--- ssosoft/__init__.py
+++ ssosoft/__init__.py
@@ -23,7 +23,7 @@ ssosoftConfig :
     including authorship, version, etc.
 """
 
-from imagers.kisipWrapper import *
+from imagers import *
 from imagers.rosaZylaCal import *
 from imagers.destretch import *
 
