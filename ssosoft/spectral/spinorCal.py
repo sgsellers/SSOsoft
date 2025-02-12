9351b7d11ec58185377009d7e11ce16a7951b587
[Sean Sellers] <[sellers@nmsu.edu]>
2025-02-11 18:20:10 -0700

    Rewrote dual-beam registration and moved to separate function. Uses dark and lamp gain corrected image, but not solar gain due to hairline concerns.

--- ssosoft/spectral/spinorCal.py
+++ ssosoft/spectral/spinorCal.py
@@ -1725,7 +1725,7 @@ class SpinorCal:
                             for specProf in range(scienceBeams.shape[2]):
                                 scienceBeams[beam, :, specProf, :] = scind.shift(
                                     scienceBeams[beam, :, specProf, :],
-                                    (0, spectralSkews[beam, spiter, specProf]),
+                                    (0, -spectralSkews[beam, spiter, specProf]),
                                     mode='nearest'
                                 )
                     # Perform alignment on deskewed beams
@@ -1969,8 +1969,8 @@ class SpinorCal:
         # Perform spectral line deskew. Do this iteratively using gain table creation line.
         x1, x2 = 20, 21
         spectralSkews = np.zeros((5, 2, deskewedDualBeams.shape[1]))
-        for spiter in range(5):
-            order = 1 if spiter < 2 else 2
+        for spiter in range(2):
+            order = 1 if spiter < 1 else 2
             for beam in range(2):
                 spectralImage = deskewedDualBeams[
                     beam, :, int(self.spinorLineCores[0] - x1):int(self.spinorLineCores[0] + x2)
@@ -1995,7 +1995,7 @@ class SpinorCal:
                 )
                 for prof in range(deskewedDualBeams.shape[1]):
                     deskewedDualBeams[beam, prof, :] = scind.shift(
-                        deskewedDualBeams[beam, prof, :], spectralSkews[spiter, beam, prof], mode='nearest'
+                        deskewedDualBeams[beam, prof, :], -spectralSkews[spiter, beam, prof], mode='nearest'
                     )
             x1 -= 3
             x2 -= 3
