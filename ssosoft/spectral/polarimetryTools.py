Corrected a couple minor errors, added reference_frame keyword to polarimetryTools.get_dst_matrix for final rotation

ssosoft/spectral/polarimetryTools.py
@@ -237,7 +237,9 @@ def check_mueller_physicality(mueller_mtx: np.ndarray) -> tuple[bool, float, flo
     return True, master_imin, master_pmin
 
 
-def get_dst_matrix(telescope_geometry: list, central_wavelength: float, matrix_file: str) -> np.ndarray:
+def get_dst_matrix(
+        telescope_geometry: list, central_wavelength: float, reference_frame: float, matrix_file: str
+) -> np.ndarray:
     """
     Gets DST telescope matrix from IDL save (2010 matrix) or numpy save (TBD, hopefully we measure it in the future)
     file. Returns the Mueller matrix of the telescope from these measurements.
@@ -248,6 +250,8 @@ def get_dst_matrix(telescope_geometry: list, central_wavelength: float, matrix_f
         3-element vector containing the coelostat azimuth, coelostat elevation, and Coude table angle
     central_wavelength : float
         In angstrom, wavelength to interpolate measured values to
+    reference_frame : float
+        In degrees, the orientation of the reference frame relative to the telescope matrix.
     matrix_file : str
         Path to telescope matrix file
 
@@ -281,7 +285,7 @@ def get_dst_matrix(telescope_geometry: list, central_wavelength: float, matrix_f
 
     entrance_window_orientation = txparams['tt'][1] * np.pi / 180
     exit_window_orientation = txparams['tt'][2] * np.pi / 180
-    ref_frame_orientation = txparams['tt'][3] * np.pi / 180
+    ref_frame_orientation = reference_frame * np.pi / 180
 
     wvls = txparams['tt'][5::7]
     entrance_window_retardance = scinterp.interp1d(
@@ -383,9 +387,9 @@ def spherical_coordinate_transform(telescope_angles: list, site_latitude: float)
     sin_x = -cos_el * sin_az
     cos_x = sin_el * cos_lat - cos_el * cos_az * sin_lat
 
-    sin_y = sin_el * sin_lat + cos_el * cos_az * sin_lat
+    sin_y = sin_el * sin_lat + cos_el * cos_az * cos_lat
     sin_z = cos_lat * sin_az
-    cos_z = sin_lat * cos_el - sin_el * sin_lat * cos_az
+    cos_z = sin_lat * cos_el - sin_el * cos_lat * cos_az
 
     x = np.arctan(sin_x / cos_x)
     y = np.arcsin(sin_y)

ssosoft/spectral/spinorCal.py
@@ -1041,8 +1041,11 @@ class SpinorCal:
         ax_lamp.imshow(
             self.lamp_gain, origin='lower', cmap='gray', vmin=0.5, vmax=2.5
         )
+        corr_flat = (self.solar_flat - self.solar_dark) / self.lamp_gain
         ax_flat.imshow(
-            (self.solar_flat - self.solar_dark) / self.lamp_gain, origin='lower', cmap='gray'
+            corr_flat, origin='lower', cmap='gray',
+            vmin=corr_flat.mean() - 2*np.std(corr_flat),
+            vmax=corr_flat.mean() + 2*np.std(corr_flat)
         )
         ax_coarse.imshow(
             self.combined_coarse_gain_table, origin='lower', cmap='gray', vmin=0.5, vmax=2.5
@@ -1970,6 +1973,7 @@ class SpinorCal:
         deskewed_dual_beams = dual_beams.copy()
 
         if self.manual_hairline_selection and hair_centers is None:
+            first_step = True
             beam0_profile = beam0.mean(axis=1)
             beam1_profile = beam1.mean(axis=1)
             beam0_range, beam1_range = spex.select_spans_doublepanel(beam0_profile, beam1_profile, 1)