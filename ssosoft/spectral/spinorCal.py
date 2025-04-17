6edda570cbf67e48b227ddb1ceab434464b3c9cb
[Sean Sellers] <[sellers@nmsu.edu]>
2025-04-17 15:34:38 -0600

    Minor bugfix

--- ssosoft/spectral/spinorCal.py
+++ ssosoft/spectral/spinorCal.py
@@ -2213,7 +2213,10 @@ class SpinorCal:
                         )
                 x1 -= 3
                 x2 -= 3
-            spex_range = np.array([[x1, x1], [x2, x2]])
+            spex_range = np.array([
+                [int(self.spinor_line_cores[0] - (x1 + 3)), int(self.spinor_line_cores[0] - (x2 + 3))],
+                [int(self.spinor_line_cores[0] - (x1 + 3)), int(self.spinor_line_cores[0] - (x2 + 3))]
+            ])
         # Find bulk spectral line center for full alignment
         spectral_center = (
             spex.find_line_core(
