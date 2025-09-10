01ce239f73bff7e7b986e1f9f92f4e4e1634f6ca
[Sean Sellers] <[sellers@nmsu.edu]>
2025-09-10 14:25:20 -0600

    SPINOR movie tweaks

--- ssosoft/tools/movie_makers.py
+++ ssosoft/tools/movie_makers.py
@@ -383,212 +383,213 @@ def spinor_movie_maker(
     progress : bool, optional
         If True, spawns a progress bar, by default True
     """
-    with fits.open(spinor_filename) as hdul:
-        scintillation = hdul["METADATA"].data["TELESCIN"]
-        timedeltas = [np.timedelta64(int(i * 1000), "ms") for i in hdul["METADATA"].data["T_ELAPSED"]]
-        timestamps = np.datetime64(hdul["METADATA"].header["TRPOS1"]) + timedeltas
-        goes_timestamps, short, long = get_goes_timeseries(timestamps[0], timestamps[-1], savedir)
-        if field_images is None:
-            iimg = np.mean(hdul["STOKES-I"].data, axis=-1)
-            ilabel = "Continuum Stokes-I"
-            qimg = np.mean(np.abs(hdul["STOKES-I"].data), axis=-1)
-            qlabel = "Mean |Stokes-Q|"
-            uimg = np.mean(np.abs(hdul["STOKES-U"].data), axis=-1)
-            ulabel = "Mean |Stokes-U|"
-            vimg = np.mean(np.abs(hdul["STOKES-V"].data), axis=-1)
-            vlabel = "Mean |Stokes-V|"
+    hdul = fits.open(spinor_filename)
+    scintillation = hdul["METADATA"].data["TELESCIN"]
+    timedeltas = [np.timedelta64(int(i * 1000), "ms") for i in hdul["METADATA"].data["T_ELAPSED"]]
+    timestamps = np.datetime64(hdul["METADATA"].header["TRPOS1"]) + timedeltas
+    goes_timestamps, short, long = get_goes_timeseries(timestamps[0], timestamps[-1], savedir)
+    if field_images is None:
+        iimg = np.mean(hdul["STOKES-I"].data, axis=-1)
+        ilabel = "Continuum Stokes-I"
+        qimg = np.mean(np.abs(hdul["STOKES-I"].data), axis=-1)
+        qlabel = "Mean |Stokes-Q|"
+        uimg = np.mean(np.abs(hdul["STOKES-U"].data), axis=-1)
+        ulabel = "Mean |Stokes-U|"
+        vimg = np.mean(np.abs(hdul["STOKES-V"].data), axis=-1)
+        vlabel = "Mean |Stokes-V|"
 
-            contimg = None
-        else:
-            iimg = field_images[0]
-            ilabel = "Line-Core Stokes-I"
-            qimg = field_images[1]
-            qlabel = "Line-Integrated Stokes-Q"
-            uimg = field_images[2]
-            ulabel = "Line-Integrated Stokes-U"
-            vimg = field_images[3]
-            vlabel = "Line-Core Deriv. Stokes-V"
+        contimg = None
+    else:
+        iimg = field_images[0]
+        ilabel = "Line-Core Stokes-I"
+        qimg = field_images[1]
+        qlabel = "Line-Integrated Stokes-Q"
+        uimg = field_images[2]
+        ulabel = "Line-Integrated Stokes-U"
+        vimg = field_images[3]
+        vlabel = "Line-Core Deriv. Stokes-V"
 
-            contimg = np.mean(hdul["STOKES-I"].data, axis=-1)
+        contimg = np.mean(hdul["STOKES-I"].data, axis=-1)
 
-        fovx = hdul["STOKES-I"].header["CDELT1"] * iimg.shape[1]
-        fovy = hdul["STOKES-I"].header["CDELT2"] * iimg.shape[0]
-        xextent = np.linspace(0, fovx, num=iimg.shape[1]) + hdul["STOKES-I"].header["CDELT1"]/2
-        wavegrid = hdul["lambda-coordinate"].data
+    fovx = hdul["STOKES-I"].header["CDELT1"] * iimg.shape[1]
+    fovy = hdul["STOKES-I"].header["CDELT2"] * iimg.shape[0]
+    xextent = np.linspace(0, fovx, num=iimg.shape[1]) + hdul["STOKES-I"].header["CDELT1"]/2
+    wavegrid = hdul["lambda-coordinate"].data
 
-        # Figure setup
-        fig = plt.figure(figsize=(12, 8))
-        gs = fig.add_gridspec(nrows=3, ncols=4)
-        if contimg is None:
-            ax_goes = fig.add_subplot(gs[2, :])
-            ax_cont = None
-        else:
-            ax_goes = fig.add_subplot(gs[2, 1:])
-            ax_cont = fig.add_subplot(gs[2, 0])
-        axi = fig.add_subplot(gs[0, 0])
-        axq = fig.add_subplot(gs[0, 1])
-        axu = fig.add_subplot(gs[1, 0])
-        axv = fig.add_subplot(gs[1, 1])
+    # Figure setup
+    fig = plt.figure(figsize=(12, 8))
+    gs = fig.add_gridspec(nrows=3, ncols=4)
+    if contimg is None:
+        ax_goes = fig.add_subplot(gs[2, :])
+        ax_cont = None
+    else:
+        ax_goes = fig.add_subplot(gs[2, 1:])
+        ax_cont = fig.add_subplot(gs[2, 0])
+    axi = fig.add_subplot(gs[0, 0])
+    axq = fig.add_subplot(gs[0, 1])
+    axu = fig.add_subplot(gs[1, 0])
+    axv = fig.add_subplot(gs[1, 1])
 
-        axi_slit = fig.add_subplot(gs[0, 2])
-        axq_slit = fig.add_subplot(gs[0, 3])
-        axu_slit = fig.add_subplot(gs[1, 2])
-        axv_slit = fig.add_subplot(gs[1, 3])
+    axi_slit = fig.add_subplot(gs[0, 2])
+    axq_slit = fig.add_subplot(gs[0, 3])
+    axu_slit = fig.add_subplot(gs[1, 2])
+    axv_slit = fig.add_subplot(gs[1, 3])
 
-        # Field image plotting
-        axi.imshow(iimg, origin="lower", cmap="gray",
-                   vmin=np.mean(iimg) - 3*np.std(iimg),
-                   vmax=np.mean(iimg) + 3*np.std(iimg),
-                   extent=(0, fovx, 0, fovy), aspect="auto")
-        axq.imshow(qimg, origin="lower", cmap="gray",
-                   vmin=np.mean(qimg) - 1.5*np.std(qimg),
-                   vmax=np.mean(qimg) + 1.5*np.std(qimg),
-                   extent=(0, fovx, 0, fovy), aspect="auto")
-        axu.imshow(uimg, origin="lower", cmap="gray",
-                   vmin=np.mean(uimg) - 1.5*np.std(uimg),
-                   vmax=np.mean(uimg) + 1.5*np.std(uimg),
-                   extent=(0, fovx, 0, fovy), aspect="auto")
-        axv.imshow(vimg, origin="lower", cmap="gray",
-                   vmin=np.mean(vimg) - 2*np.std(vimg),
-                   vmax=np.mean(vimg) + 2*np.std(vimg),
-                   extent=(0, fovx, 0, fovy), aspect="auto")
+    # Field image plotting
+    axi.imshow(iimg, origin="lower", cmap="gray",
+                vmin=np.mean(iimg) - 3*np.std(iimg),
+                vmax=np.mean(iimg) + 3*np.std(iimg),
+                extent=(0, fovx, 0, fovy), aspect="auto")
+    axq.imshow(qimg, origin="lower", cmap="gray",
+                vmin=np.mean(qimg) - 1.5*np.std(qimg),
+                vmax=np.mean(qimg) + 1.5*np.std(qimg),
+                extent=(0, fovx, 0, fovy), aspect="auto")
+    axu.imshow(uimg, origin="lower", cmap="gray",
+                vmin=np.mean(uimg) - 1.5*np.std(uimg),
+                vmax=np.mean(uimg) + 1.5*np.std(uimg),
+                extent=(0, fovx, 0, fovy), aspect="auto")
+    axv.imshow(vimg, origin="lower", cmap="gray",
+                vmin=np.mean(vimg) - 2*np.std(vimg),
+                vmax=np.mean(vimg) + 2*np.std(vimg),
+                extent=(0, fovx, 0, fovy), aspect="auto")
 
-        axi.set_title(ilabel, weight="bold")
-        axq.set_title(qlabel, weight="bold")
-        axu.set_title(ulabel, weight="bold")
-        axv.set_title(vlabel, weight="bold")
-        axi.set_ylabel("Extent [asec]")
-        axu.set_ylabel("Extent [asec]")
-        axu.set_xlabel("Extent [asec]")
-        axv.set_xlabel("Extent [asec]")
+    axi.set_title(ilabel, weight="bold")
+    axq.set_title(qlabel, weight="bold")
+    axu.set_title(ulabel, weight="bold")
+    axv.set_title(vlabel, weight="bold")
+    axi.set_ylabel("Extent [asec]")
+    axu.set_ylabel("Extent [asec]")
+    axu.set_xlabel("Extent [asec]")
+    axv.set_xlabel("Extent [asec]")
 
+    if ax_cont is not None:
+        ax_cont.imshow(contimg, origin="lower", cmap="gray",
+                        vmin=np.mean(contimg) - 3*np.std(contimg),
+                        vmax=np.mean(contimg) + 3*np.std(contimg),
+                        extent=(0, fovx, 0, fovy), aspect="auto")
+        ax_cont.set_title("Continuum Stokes-I", weight="bold")
+        ax_cont.set_ylabel("Extent [asec]")
+        ax_cont.set_xlabel("Extent [asec]")
+
+    iline = axi.axvline(xextent[0], c="C1", ls="-", lw=1, zorder=10)
+    qline = axq.axvline(xextent[0], c="C1", ls="-", lw=1, zorder=10)
+    uline = axu.axvline(xextent[0], c="C1", ls="-", lw=1, zorder=10)
+    vline = axv.axvline(xextent[0], c="C1", ls="-", lw=1, zorder=10)
+    if ax_cont is not None:
+        cline = ax_cont.axvline(xextent[0], c="C1", ls="-", lw=1, zorder=10)
+    else:
+        cline = None
+
+    # Slit image plotting
+    islit = hdul["STOKES-I"].data[:, 0, :]
+    qslit = hdul["STOKES-Q"].data[:, 0, :]
+    uslit = hdul["STOKES-U"].data[:, 0, :]
+    vslit = hdul["STOKES-V"].data[:, 0, :]
+
+    i_s = axi_slit.imshow(islit, origin="lower", cmap="gray",
+                            vmin=np.mean(islit) - 3*np.std(islit),
+                            vmax=np.mean(islit) + 3*np.std(islit),
+                            extent=(0, fovy, wavegrid[0], wavegrid[-1]),
+                            aspect="auto")
+    q_s = axq_slit.imshow(qslit, origin="lower", cmap="gray",
+                            vmin=np.mean(qslit) - 2*np.std(qslit),
+                            vmax=np.mean(qslit) + 2*np.std(qslit),
+                            extent=(0, fovy, wavegrid[0], wavegrid[-1]),
+                            aspect="auto")
+    u_s = axu_slit.imshow(uslit, origin="lower", cmap="gray",
+                            vmin=np.mean(uslit) - 2*np.std(uslit),
+                            vmax=np.mean(uslit) + 2*np.std(uslit),
+                            extent=(0, fovy, wavegrid[0], wavegrid[-1]),
+                            aspect="auto")
+    v_s = axv_slit.imshow(vslit, origin="lower", cmap="gray",
+                            vmin=np.mean(vslit) - 2*np.std(vslit),
+                            vmax=np.mean(vslit) + 2*np.std(vslit),
+                            extent=(0, fovy, wavegrid[0], wavegrid[-1]),
+                            aspect="auto")
+
+    axi_slit.set_title("Stokes-I", weight="bold")
+    axi_slit.set_ylabel("Extent [asec]")
+    axq_slit.set_title("Stokes-Q", weight="bold")
+    axu_slit.set_title("Stokes-U", weight="bold")
+    axu_slit.set_ylabel("Extent [asec]")
+    axu_slit.set_xlabel("Wavelength [$\\AA$]")
+    axv_slit.set_title("Stokes-V", weight="bold")
+    axv_slit.set_xlabel("Wavelength [$\\AA$]")
+
+
+    # GOES/Scintillation plot
+    ax_goes.plot(timestamps, scintillation, c="C0")
+    ax_goes.set_xlim(timestamps[0], timestamps[-1])
+    ax_goes.xaxis.set_major_formatter(mdates.DateFormatter("%H:%M"))
+    ax_goes.set_ylabel("Scintillation", color="C0", weight="bold")
+    ax_goes.tick_params(axis="y", labelcolor="C0")
+    ax_goes.set_title("Telescope Scintillation/GOES Lightcuve", weight="bold")
+
+    lc_vline = ax_goes.axvline(timestamps[0], ls="-", color="C2", lw=1, zorder=10)
+    if len(goes_timestamps) > 0:
+        ax_goes2 = ax_goes.twinx()
+        ax_goes2.plot(goes_timestamps, long, color="C1")
+        ax_goes2.set_ylim(-9, -2)
+        ax_goes2.set_yticks([-7.5, -6.5, -5.5, -4.5, -3.5],
+                            labels=["A", "B", "C", "M", "X"])
+        ax_goes2.set_ylabel("GOES LC", weight="bold", color="C1")
+        ax_goes2.tick_params(axis="y", labelcolor="C1")
+
+    title = fig.suptitle(f"SPINOR {central_wavelength}: {str(timestamps[0])}", weight="bold")
+    fig.tight_layout()
+
+    def animate_spectropolarimeter(i):
+        # Update line positions
+        iline.set_xdata([xextent[i], xextent[i]])
+        qline.set_xdata([xextent[i], xextent[i]])
+        uline.set_xdata([xextent[i], xextent[i]])
+        vline.set_xdata([xextent[i], xextent[i]])
         if ax_cont is not None:
-            ax_cont.imshow(contimg, origin="lower", cmap="gray",
-                           vmin=np.mean(contimg) - 3*np.std(contimg),
-                           vmax=np.mean(contimg) + 3*np.std(contimg),
-                           extent=(0, fovx, 0, fovy), aspect="auto")
-            ax_cont.set_title("Continuum Stokes-I", weight="bold")
-            ax_cont.set_ylabel("Extent [asec]")
-            ax_cont.set_xlabel("Extent [asec]")
+            cline.set_xdata([xextent[i], xextent[i]])
+        lc_vline.set_xdata([timestamps[i], timestamps[i]])
+        # Update Slit Images
+        islit = hdul["STOKES-I"].data[:, i, :]
+        qslit = hdul["STOKES-Q"].data[:, i, :]
+        uslit = hdul["STOKES-U"].data[:, i, :]
+        vslit = hdul["STOKES-V"].data[:, i, :]
+        i_s.set_array(islit)
+        q_s.set_array(qslit)
+        u_s.set_array(uslit)
+        v_s.set_array(vslit)
+        inorm = Normalize(np.mean(islit) - 3*np.std(islit))
+        i_s.set_norm(inorm)
+        qnorm = Normalize(np.mean(qslit) - 2*np.std(qslit))
+        q_s.set_norm(qnorm)
+        unorm = Normalize(np.mean(uslit) - 2*np.std(uslit))
+        u_s.set_norm(unorm)
+        vnorm = Normalize(np.mean(vslit) - 2*np.std(vslit))
+        v_s.set_norm(vnorm)
+        title.set_text(f"SPINOR {central_wavelength}: {str(timestamps[i])}")
+        return
 
-        iline = axi.axvline(xextent[0], c="C1", ls="-", lw=1, zorder=10)
-        qline = axq.axvline(xextent[0], c="C1", ls="-", lw=1, zorder=10)
-        uline = axu.axvline(xextent[0], c="C1", ls="-", lw=1, zorder=10)
-        vline = axv.axvline(xextent[0], c="C1", ls="-", lw=1, zorder=10)
-        if ax_cont is not None:
-            cline = ax_cont.axvline(xextent[0], c="C1", ls="-", lw=1, zorder=10)
+    if fps == 0:
+        # Best guess: We want to start around 6 fps
+        # 6 fps gives a maximum 1.5 minute movie for a 550-step map
+        # Want a minimum movie length of 30 seconds
+        # So slower if the desired movie is < 30 seconds
+        if iimg.shape[1] / 8 < 30:
+            fps = iimg.shape[1] / 30
         else:
-            cline = None
+            fps = 6
+    anim = animation.FuncAnimation(
+        fig, animate_spectropolarimeter,
+        frames=tqdm.tqdm(
+            range(iimg.shape[1]),
+            file=sys.stdout,
+            disable=not progress,
+            desc="Creating SPINOR movie..."
+        ),
+        interval=1000 / fps,
+        init_func=lambda: None
+    )
 
-        # Slit image plotting
-        islit = hdul["STOKES-I"].data[:, 0, :]
-        qslit = hdul["STOKES-Q"].data[:, 0, :]
-        uslit = hdul["STOKES-U"].data[:, 0, :]
-        vslit = hdul["STOKES-V"].data[:, 0, :]
-
-        i_s = axi_slit.imshow(islit, origin="lower", cmap="gray",
-                             vmin=np.mean(islit) - 3*np.std(islit),
-                             vmax=np.mean(islit) + 3*np.std(islit),
-                             extent=(0, fovy, wavegrid[0], wavegrid[-1]),
-                             aspect="auto")
-        q_s = axq_slit.imshow(qslit, origin="lower", cmap="gray",
-                             vmin=np.mean(qslit) - 2*np.std(qslit),
-                             vmax=np.mean(qslit) + 2*np.std(qslit),
-                             extent=(0, fovy, wavegrid[0], wavegrid[-1]),
-                             aspect="auto")
-        u_s = axu_slit.imshow(uslit, origin="lower", cmap="gray",
-                             vmin=np.mean(uslit) - 2*np.std(uslit),
-                             vmax=np.mean(uslit) + 2*np.std(uslit),
-                             extent=(0, fovy, wavegrid[0], wavegrid[-1]),
-                             aspect="auto")
-        v_s = axv_slit.imshow(vslit, origin="lower", cmap="gray",
-                             vmin=np.mean(vslit) - 2*np.std(vslit),
-                             vmax=np.mean(vslit) + 2*np.std(vslit),
-                             extent=(0, fovy, wavegrid[0], wavegrid[-1]),
-                             aspect="auto")
-
-        axi_slit.set_title("Stokes-I", weight="bold")
-        axi_slit.set_ylabel("Extent [asec]")
-        axq_slit.set_title("Stokes-Q", weight="bold")
-        axu_slit.set_title("Stokes-U", weight="bold")
-        axu_slit.set_ylabel("Extent [asec]")
-        axu_slit.set_xlabel("Wavelength [$\\AA$]")
-        axv_slit.set_title("Stokes-V", weight="bold")
-        axv_slit.set_xlabel("Wavelength [$\\AA$]")
-
-
-        # GOES/Scintillation plot
-        ax_goes.plot(timestamps, scintillation, c="C0")
-        ax_goes.set_xlim(timestamps[0], timestamps[-1])
-        ax_goes.xaxis.set_major_formatter(mdates.DateFormatter("%H:%M"))
-        ax_goes.set_ylabel("Scintillation", color="C0", weight="bold")
-        ax_goes.tick_params(axis="y", labelcolor="C0")
-        ax_goes.set_title("Telescope Scintillation/GOES Lightcuve", weight="bold")
-
-        lc_vline = ax_goes.axvline(timestamps[0], ls="-", color="C2", lw=1, zorder=10)
-        if len(goes_timestamps) > 0:
-            ax_goes2 = ax_goes.twinx()
-            ax_goes2.plot(goes_timestamps, long, color="C1")
-            ax_goes2.set_ylim(-9, -2)
-            ax_goes2.set_yticks([-7.5, -6.5, -5.5, -4.5, -3.5],
-                                labels=["A", "B", "C", "M", "X"])
-            ax_goes2.set_ylabel("GOES LC", weight="bold", color="C1")
-            ax_goes2.tick_params(axis="y", labelcolor="C1")
-
-        title = fig.suptitle(f"SPINOR {central_wavelength}: {str(timestamps[0])}", weight="bold")
-        fig.tight_layout()
-
-        def animate_spectropolarimeter(i):
-            # Update line positions
-            iline.set_xdata([xextent[i], xextent[i]])
-            qline.set_xdata([xextent[i], xextent[i]])
-            uline.set_xdata([xextent[i], xextent[i]])
-            vline.set_xdata([xextent[i], xextent[i]])
-            if ax_cont is not None:
-                cline.set_xdata([xextent[i], xextent[i]])
-            lc_vline.set_xdata([timestamps[i], timestamps[i]])
-            # Update Slit Images
-            islit = hdul["STOKES-I"].data[:, i, :]
-            qslit = hdul["STOKES-Q"].data[:, i, :]
-            uslit = hdul["STOKES-U"].data[:, i, :]
-            vslit = hdul["STOKES-V"].data[:, i, :]
-            i_s.set_array(islit)
-            q_s.set_array(qslit)
-            u_s.set_array(uslit)
-            v_s.set_array(vslit)
-            inorm = Normalize(np.mean(islit) - 3*np.std(islit))
-            i_s.set_norm(inorm)
-            qnorm = Normalize(np.mean(qslit) - 2*np.std(qslit))
-            q_s.set_norm(qnorm)
-            unorm = Normalize(np.mean(uslit) - 2*np.std(uslit))
-            u_s.set_norm(unorm)
-            vnorm = Normalize(np.mean(vslit) - 2*np.std(vslit))
-            v_s.set_norm(vnorm)
-            title.set_text(f"SPINOR {central_wavelength}: {str(timestamps[i])}")
-            return
-
-        if fps == 0:
-            # Best guess: We want to start around 6 fps
-            # 6 fps gives a maximum 1.5 minute movie for a 550-step map
-            # Want a minimum movie length of 30 seconds
-            # So slower if the desired movie is < 30 seconds
-            if iimg.shape[1] / 8 < 30:
-                fps = iimg.shape[1] / 30
-            else:
-                fps = 6
-        anim = animation.FuncAnimation(
-            fig, animate_spectropolarimeter,
-            frames=tqdm.tqdm(
-                range(iimg.shape[1]),
-                file=sys.stdout,
-                disable=not progress,
-                desc="Creating SPINOR movie..."
-            ),
-            interval=1000 / fps,
-            init_func=lambda: None
-        )
-
-        anim.save(os.path.join(savedir, savename), fps=fps)
+    anim.save(os.path.join(savedir, savename), fps=fps)
+    hdul.close()
     return
 
 def get_goes_timeseries(starttime, endtime, outpath):
